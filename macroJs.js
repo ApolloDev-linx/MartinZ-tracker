d3d2d73d3e52cc4b53da04750539692fd403f25e
ApolloDev-linx <bossangel1127@gmail.com>
2025-06-16 16:24:38 -0500

    Added font styling and button log function for meal input

--- macroJs.js
+++ macroJs.js
@@ -1,1 +1,19 @@
-console.log("PLUG")
+function logMeal(){
+    const meal = document.getElementById("enterMeal").value;
+    const calories = document.getElementById("calories").value;
+    const fats = document.getElementById("fats").value;
+    const protein = document.getElementById("protein").value;
+    const carbs = document.getElementById("carbs").value;
+
+    console.log("Meal:", meal);
+    console.log("Calories:", calories);
+    console.log("Fats:", fats);
+    console.log("Protein:", protein);
+    console.log("Carbs:", carbs);
+    document.addEventListenr('DOMContentLoaded', () =>{	
+	let btnLog = document.getElementById("logBtn");
+	btnLog.addEventListener('click', logMeal );
+
+})
+} 
+
