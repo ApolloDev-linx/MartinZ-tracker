bbcc9e8f255d88144b387c24d8be8bc03d59ef27
ApolloDev-linx <bossangel1127@gmail.com>
2025-06-23 16:36:03 -0500

    Documented Flask backend logic and data flow for macroTracker

--- macroTracker.py
+++ macroTracker.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-app = Flask(__name__)  
+#allows acces from other origins.
+app = Flask(__name__)
+#this activates CORS rules on app.
 CORS(app)
-
+#this is decorator essentially saying when this route /mealInput is accessed run the function below.
 @app.route('/mealInput', methods=['POST'])
+
 def log_meal():
+# the definition below grabs incoming data from the frontend form or payload and stores it in our var called data.    
     data = request.form
     meal = data.get('enterMeal')
     fats = float(data.get('fats', 0))
@@ -12,13 +16,13 @@ def log_meal():
     carbs = float(data.get('carbs',0))
     calories = float(data.get('cal',0))
 
-    #Divide each by 7 in a var called daily_name
+    #Divide each by 7 in a var called daily_name.
 
     daily_fats = fats / 7
     daily_protein = protein / 7
     daily_carbs = carbs / 7
     daily_calories = calories / 7
-
+#here we turn the python dict into a json response.
     return jsonify ({
         "meal": meal,
         "dailyFats": round(daily_fats, 2),
@@ -27,27 +31,31 @@ def log_meal():
         "dailyCalories": round(daily_calories, 2)
             
         })
+    
 @app.route('/login', methods=['GET'])
+# using render template we send the html file we mention to the page browser.
 def serve_login():
     return render_template('macroLogin.html')
 
 @app.route('/login', methods=['POST'])
 def login():
+# we then take raw json data and turn it into a python dict.    
     data = request.get_json()
     username= data['username']
     password= data['password']
-
+#we check if the key has the value we hardcoded here.
     if username == "apollo" and password == "test123":
+        #sends a true or false to our loginJS.js
         return jsonify({'success': True})
     else:
         return jsonify({'success': False})
-
+#same logic from line 36
 @app.route('/main')
 def main_page():
     return render_template('mainMacro.html')
 
 
-
+#if we are running the file directly we run the app: python macroTracker.py
 if __name__ == '__main__':
     app.run(debug=True)
 
